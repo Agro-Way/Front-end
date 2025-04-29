a6c85d49a5d273a6a5c99e4b339d51bcae144972
Jenilson Francisco <farnciscoviwaldo@gmail.com>
2025-04-29 13:26:53 +0100

    feat: first configurations

---
+++ src/globalStyles/global-styles.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: "National Park", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+  }
+
+  input {
+    border: none;
+  }
+
+  ::-webkit-scrollbar {
+    width: 0.4rem;
+  }
+
+  ::-webkit-scrollbar-track {
+    background:rgb(28, 34, 21);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 2rem;
+    background:rgb(26, 46, 20);
+  }
+
+`
