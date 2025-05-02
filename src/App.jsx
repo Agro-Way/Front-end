8dd6f999b7bbe51510263be159ae68fff6f43b17
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-02 13:52:10 +0100

    Subindo arquivos do layout do projrto
    Foi feito um upload do layout do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Blog from './pages/Blog';
+import Produtos from './pages/Produtos';
+import Contato from './pages/Contato';
+import Post from './pages/Post';
+import NotFound from './pages/NotFound';
+import Teste from './pages/Teste';
+import DashboardHome from './dashboard/DashboardHome';
+import './assets/css/style.css'; // estilo global
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/*Rotas do site principal*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/post/" element={<Post />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/teste" element={<Teste />} />
+
+        {/*Rotas do dashboard*/}
+        <Route path="/dashboard" element={<DashboardHome />} />
+
+        {/*Rotas não encontradas*/}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,113 @@
+// src/pages/Sobre.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import sobreImg from "../assets/img/sobre2.jpg";
+import sobreFocus1 from "../assets/img/focus-1.jpg";
+import sobreFocus2 from "../assets/img/focus-2.jpg";
+import sobreFocus3 from "../assets/img/focus-3.jpg";
+import './../assets/css/sobre.css';
+
+function Sobre() {
+  useDocumentTitle("Sobre | Agroway");
+
+  return (
+    <>
+      {/*Header*/}
+      <Header />
+
+      {/*sub-heading*/}
+      <section class="sub-heading">
+        <h1>Sobre Nós</h1>
+      </section>
+      
+      {/*about*/}
+      <section className="about">
+        <div className="row">
+          <div className="image">
+            <img src={sobreImg} alt="Sobre nós" title="Sobre nós" />
+          </div>
+          <div className="content">
+            <h3>Seja Bem-Vindo A <span>Agroway</span></h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Esse nesciunt impedit voluptatibus! Ipsam veniam, amet aliquid mollitia odio, 
+              aliquam iure error deserunt sint nobis eius vitae, blanditiis ex commodi molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Perspiciatis consequatur nostrum culpa rem debitis sequi accusantium pariatur placeat, facere, accusamus ullam. Facilis alias consequuntur natus voluptates. 
+              Commodi incidunt asperiores a.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/*section-about*/}
+      <section className="section-about">
+        <div className="content">
+          <h3>IMPULSIONE SEU NEGÓCIO NA AGROWAY</h3>
+          <p>
+            Plataforma de qualidade significativa para o seu negócio, conectando produtores, motoristas e clientes.
+          </p>
+          <Link to="#" className="btn">Cadastrar</Link>
+        </div>
+      </section>
+
+      {/*focus*/}
+      <section className="focus">
+        <h3 className="heading">Nossas <span>Prioridades</span></h3>
+
+        <div className="box-container">
+          <div class="box">
+            <div class="image">
+              <img src={sobreFocus1} alt="Prioridade 1: Clientes"/>
+              <div class="share">
+                <a href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora"></a>
+                <a href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp"></a>
+                <a href="#" class="fab fa-instagram" title="Ver Instagram"></a>
+                </div>
+            </div>
+            <div class="content">
+              <h3>Clientes</h3>
+              <h5>Agroway</h5>
+            </div>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src={sobreFocus2}  alt="Prioridade 2: Motoristas"/>
+              <div class="share">
+                <a href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora"></a>
+                <a href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp"></a>
+                <a href="#" class="fab fa-instagram" title="Ver Instagram"></a>
+                </div>
+            </div>
+            <div class="content">
+              <h3>Motoristas</h3>
+              <h5>Agroway</h5>
+            </div>
+          </div>
+          <div class="box">
+            <div class="image">
+              <img src={sobreFocus3}  alt="Prioridade 3: Produtores"/>
+              <div class="share">
+                <a href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora"></a>
+                <a href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp"></a>
+                <a href="#" class="fab fa-instagram" title="Ver Instagram"></a>
+                </div>
+            </div>
+            <div class="content">
+              <h3>Produtores</h3>
+              <h5>Agroway</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+
+      {/*footer*/}
+      <Footer />
+    </>
+  );
+}
+
+export default Sobre;
