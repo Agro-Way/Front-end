661df773ff200154dd880d9b59b0e6c12730c55d
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-26 22:37:59 +0100

    Adição da rota no cabeçalho do site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,16 +44,17 @@ function Header() {
         <Link to="#"
           className="fas fa-user"
           id="login-btn"
-        ></Link>
-        <Link to="#"
+        />
+        <Link to="/carrinho"
           className="fas fa-shopping-cart"
           id="cart-btn"
         ><span className="count">0</span></Link>
+        {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
         <div
           className={`fas ${menuAtivo ? "fa-times" : "fa-bars"}`}
           id="menu"
           onClick={toggleMenu}
-        ></div>
+        />
       </div>
     </header>
   );
