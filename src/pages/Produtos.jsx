98ad926a4cdc808088e2c964267cad49e3744043
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-29 10:01:45 +0100

    Várias alterações feitas

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -4,6 +4,7 @@ import useDocumentTitle from "../hooks/useDocumentTitle";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import CategorySlider from "../components/CategorySlider";
 
 function Produtos() {
   useDocumentTitle("Produtos | Agroway");
@@ -14,9 +15,11 @@ function Produtos() {
       <Header />
 
       {/*sub-heading*/}
-      <section class="sub-heading">
+      <section className="sub-heading">
         <h1>Nossos Produtos</h1>
       </section>
+
+      <CategorySlider />
       
       {/*produtos*/}
       <section className="product">
@@ -110,23 +113,23 @@ function Produtos() {
 
       {/*paginação*/}
       <section>
-        <div class="d-flex">
-          <nav class="nav-page">
-            <ul class="pagination">
-              <li class="page-item">
-                <Link class="page-link" href="#" title="Anterior">
-                  <i class="fas fa-chevron-left" />
+        <div className="d-flex">
+          <nav className="nav-page">
+            <ul className="pagination">
+              <li className="page-item">
+                <Link className="page-link" to="#" title="Anterior">
+                  <i className="fas fa-chevron-left" />
                 </Link >
               </li>
-              <li class="page-item"><Link class="page-link" href="#">1</Link ></li>
-              <li class="page-item"><Link class="page-link" href="#">2</Link ></li>
-              <li class="page-item"><Link class="page-link" href="#">3</Link ></li>
-              <li class="page-item"><Link class="page-link" href="#">4</Link ></li>
-              <li class="page-item"><Link class="page-link" href="#">5</Link ></li>
-              <li class="page-item"><Link class="page-link" href="#">6</Link ></li>
-              <li class="page-item">
-                <Link  class="page-link" href="#" title="Próximo">
-                  <i class="fas fa-chevron-right" />
+              <li className="page-item"><Link class="page-link" to="#">1</Link ></li>
+              <li className="page-item"><Link class="page-link" to="#">2</Link ></li>
+              <li className="page-item"><Link class="page-link" to="#">3</Link ></li>
+              <li className="page-item"><Link class="page-link" to="#">4</Link ></li>
+              <li className="page-item"><Link class="page-link" to="#">5</Link ></li>
+              <li className="page-item"><Link class="page-link"to="#">6</Link ></li>
+              <li className="page-item">
+                <Link  className="page-link" to="#" title="Próximo">
+                  <i className="fas fa-chevron-right" />
                 </Link >
               </li>
             </ul>
