646a7e8e210dbee1aca27d98ec183ad233ac5328
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-04 23:31:19 +0100

    Imagem da página removida

--- src/pages/RecuperarConta.jsx
+++ src/pages/RecuperarConta.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useDocumentTitle from "../hooks/useDocumentTitle";
-import sobreFocus3 from "../assets/img/focus-3.jpg";
 import './../assets/css/login.css';
 //import LoginStyle from '@/assets/css/LogintStyle';
 
