edf18ae01980ad028d37847057c377c732945f33
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-04 15:06:03 +0100

    Página de login foi criada

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+// src/pages/Login.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import sobreFocus3 from "../assets/img/focus-3.jpg";
+//import './../assets/css/login.css';
+//import LoginStyle from '@/assets/css/LogintStyle';
+
+function Login() {
+  useDocumentTitle("Login | Agroway");
+
+  return (
+    <>
+      {/*login*/}
+      <section className="login">
+      </section>
+    </>
+  );
+}
+
+export default Login;
