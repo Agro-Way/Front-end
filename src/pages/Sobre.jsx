5cb716cfdf61e4c1f7c9647e9acfcf02128dbebe
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-06 23:52:38 +0100

    Seção de contato adicionado

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,18 +1,21 @@
 // src/pages/Sobre.jsx
-import React from "react";
-import { Link } from "react-router-dom";
-import useDocumentTitle from "../hooks/useDocumentTitle";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import sobreImg from "../assets/img/sobre2.jpg";
-import sobreFocus1 from "../assets/img/focus-1.jpg";
-import sobreFocus2 from "../assets/img/focus-2.jpg";
-import sobreFocus3 from "../assets/img/focus-3.jpg";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import useDocumentTitle from '../hooks/useDocumentTitle'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import sobreImg from '../assets/img/sobre2.jpg'
+import sobreFocus1 from '../assets/img/focus-1.jpg'
+import sobreFocus2 from '../assets/img/focus-2.jpg'
+import sobreFocus3 from '../assets/img/focus-3.jpg'
+import contactImg from "../assets/img/contact.png";
 //import './../assets/css/sobre.css';
-import AboutStyle from '@/assets/css/AboutStyle';
+// import './../assets/css/contato.css'
+import AboutStyle from '@/assets/css/AboutStyle'
+import ContactStyle from '@/assets/css/ContactStyle'
 
 function Sobre() {
-  useDocumentTitle("Sobre | Agroway");
+  useDocumentTitle('Sobre | Agroway')
 
   return (
     <>
@@ -20,10 +23,10 @@ function Sobre() {
       <Header />
 
       {/*sub-heading*/}
-      <section class="sub-heading">
+      <section className="sub-heading">
         <h1>Sobre Nós</h1>
       </section>
-      
+
       {/*about*/}
       <section className="about">
         <div className="row">
@@ -31,12 +34,17 @@ function Sobre() {
             <img src={sobreImg} alt="Sobre nós" title="Sobre nós" />
           </div>
           <div className="content">
-            <h3>Seja Bem-Vindo A <span>Agroway</span></h3>
+            <h3>
+              Seja Bem-Vindo A <span>Agroway</span>
+            </h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-              Esse nesciunt impedit voluptatibus! Ipsam veniam, amet aliquid mollitia odio, 
-              aliquam iure error deserunt sint nobis eius vitae, blanditiis ex commodi molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Perspiciatis consequatur nostrum culpa rem debitis sequi accusantium pariatur placeat, facere, accusamus ullam. Facilis alias consequuntur natus voluptates. 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
+              nesciunt impedit voluptatibus! Ipsam veniam, amet aliquid mollitia
+              odio, aliquam iure error deserunt sint nobis eius vitae,
+              blanditiis ex commodi molestias! Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Perspiciatis consequatur nostrum
+              culpa rem debitis sequi accusantium pariatur placeat, facere,
+              accusamus ullam. Facilis alias consequuntur natus voluptates.
               Commodi incidunt asperiores a.
             </p>
           </div>
@@ -48,55 +56,87 @@ function Sobre() {
         <div className="content">
           <h3>IMPULSIONE SEU NEGÓCIO NA AGROWAY</h3>
           <p>
-            Plataforma de qualidade significativa para o seu negócio, conectando produtores, motoristas e clientes.
+            Plataforma de qualidade significativa para o seu negócio, conectando
+            produtores, motoristas e clientes.
           </p>
-          <Link to="/cadastrar" className="btn">Cadastrar</Link>
+          <Link to="/cadastrar" className="btn">
+            Cadastrar
+          </Link>
         </div>
       </AboutStyle>
 
       {/*focus*/}
       <AboutStyle className="focus">
-        <h3 className="heading">Nossas <span>Prioridades</span></h3>
+        <h3 className="heading">
+          Nossas <span>Prioridades</span>
+        </h3>
 
         <div className="box-container">
-          <div class="box">
-            <div class="image">
-              <img src={sobreFocus1} alt="Prioridade 1: Clientes"/>
-              <div class="share">
-                <Link href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora" />
-                <Link  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp" />
-                <Link  href="#" class="fab fa-instagram" title="Ver Instagram" />
+          <div className="box">
+            <div className="image">
+              <img src={sobreFocus1} alt="Prioridade 1: Clientes" />
+              <div className="share">
+                <Link
+                  href="tel:+244 999999999"
+                  className="fas fa-phone"
+                  title="Ligar Agora"
+                />
+                <Link
+                  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi"
+                  target="_blank"
+                  className="fab fa-whatsapp"
+                  title="Enviar mensagem via Whatsapp"
+                />
+                <Link href="#" className="fab fa-instagram" title="Ver Instagram" />
               </div>
             </div>
-            <div class="content">
+            <div className="content">
               <h3>Clientes</h3>
               <h5>Agroway</h5>
             </div>
           </div>
-          <div class="box">
-            <div class="image">
-              <img src={sobreFocus2}  alt="Prioridade 2: Motoristas"/>
-              <div class="share">
-                <Link href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora" />
-                <Link  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp" />
-                <Link  href="#" class="fab fa-instagram" title="Ver Instagram" />
+          <div className="box">
+            <div className="image">
+              <img src={sobreFocus2} alt="Prioridade 2: Motoristas" />
+              <div className="share">
+                <Link
+                  href="tel:+244 999999999"
+                  className="fas fa-phone"
+                  title="Ligar Agora"
+                />
+                <Link
+                  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi"
+                  target="_blank"
+                  className="fab fa-whatsapp"
+                  title="Enviar mensagem via Whatsapp"
+                />
+                <Link href="#" className="fab fa-instagram" title="Ver Instagram" />
               </div>
             </div>
-            <div class="content">
+            <div className="content">
               <h3>Motoristas</h3>
               <h5>Agroway</h5>
             </div>
           </div>
-          <div class="box">
-            <div class="image">
-              <img src={sobreFocus3}  alt="Prioridade 3: Produtores"/>
-              <div class="share">
-                <Link href="tel:+244 999999999" class="fas fa-phone" title="Ligar Agora" />
-                <Link  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi" target="_blank" class="fab fa-whatsapp" title="Enviar mensagem via Whatsapp" />
-                <Link  href="#" class="fab fa-instagram" title="Ver Instagram" />
+          <div className="box">
+            <div className="image">
+              <img src={sobreFocus3} alt="Prioridade 3: Produtores" />
+              <div className="share">
+                <Link
+                  href="tel:+244 999999999"
+                  className="fas fa-phone"
+                  title="Ligar Agora"
+                />
+                <Link
+                  href="https://api.whatsapp.com/send/?phone=244999999999&text=Oi"
+                  target="_blank"
+                  className="fab fa-whatsapp"
+                  title="Enviar mensagem via Whatsapp"
+                />
+                <Link href="#" className="fab fa-instagram" title="Ver Instagram" />
               </div>
             </div>
-            <div class="content">
+            <div className="content">
               <h3>Produtores</h3>
               <h5>Agroway</h5>
             </div>
@@ -104,10 +144,92 @@ function Sobre() {
         </div>
       </AboutStyle>
 
+      {/*infos*/}
+      <ContactStyle className="infos">
+        <h1 className="heading">
+          Nos <span>Contate</span>
+        </h1>
+
+        <div className="info-container">
+          <div className="info">
+            <i className="fas fa-clock" />
+            <h3>Hora De Abertura</h3>
+            <p>Segunda - Quinta: 08:00 - 16:00</p>
+            <p>Sexta-feira: 09:00 - 14:00</p>
+          </div>
+
+          <div className="info">
+            <i className="fas fa-phone" />
+            <h3>Nosso Telefone</h3>
+            <p>+244 999999999</p>
+          </div>
+
+          <div className="info">
+            <i className="fas fa-map-marker-alt" />
+            <h3>Nosso Endereço</h3>
+            <address>Angola, Luanda</address>
+          </div>
+        </div>
+      </ContactStyle>
+
+      {/*contato*/}
+      <ContactStyle className="contact">
+        <h1 className="heading">
+          Preencha O <span>Formulário</span>
+        </h1>
+
+        <div className="row">
+          <div className="image">
+            <img src={contactImg} alt="Nos Contacte" />
+          </div>
+
+          <form action="" className="form">
+            <div className="inputBox">
+              <input type="text" name="nome" placeholder="Seu nome" required />
+              <input
+                type="text"
+                name="email"
+                placeholder="exemplo@gmail.com"
+                required
+              />
+            </div>
+
+            <div className="inputBox">
+              <input
+                type="tel"
+                name="tel"
+                placeholder="Seu telefone"
+                required
+              />
+              <input
+                type="text"
+                name="assunto"
+                placeholder="Seu assunto"
+                required
+              />
+            </div>
+
+            <div className="inputBox">
+              <textarea
+                name="msg"
+                placeholder="Sua mensagem..."
+                cols="30"
+                rows="10"
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn">
+              Enviar Mensagem
+            </button>
+          </form>
+        </div>
+      </ContactStyle>
+
       {/*footer*/}
       <Footer />
     </>
-  );
+  )
 }
 
-export default Sobre;
+export default Sobre
