351c4a96cf1bc4ac1f8ee66a5ac9d26afe1c33bd
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-09 15:12:24 +0100

    Modal ficou totalmente responsivo

--- src/dashboard/components/common/FullScreenModal.jsx
+++ src/dashboard/components/common/FullScreenModal.jsx
@@ -1,44 +1,56 @@
 import { X } from "lucide-react";
-import { motion, AnimatePresence } from "framer-motion";
-import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
 
-export default function FullScreenModal({ onClose, title, children }) {
+export default function FullscreenModal({ onClose, title, children }) {
 	const backdropRef = useRef(null);
 
 	const handleBackdropClick = (e) => {
-		// Fecha o modal se clicar fora do conteúdo
 		if (e.target === backdropRef.current) {
 			onClose();
 		}
 	};
 
+	useEffect(() => {
+		document.body.classList.add("overflow-hidden");
+		return () => {
+			document.body.classList.remove("overflow-hidden");
+		};
+	}, []);
+
 	return (
 		<AnimatePresence>
 			<motion.div
 				ref={backdropRef}
-				className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+				className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 overflow-x-hidden"
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				exit={{ opacity: 0 }}
 				onClick={handleBackdropClick}
 			>
 				<motion.div
-					className="relative  w-full  h-full bg-gray-800 sm:rounded-lg sm:max-w-3xl sm:h-auto sm:p-6 p-4 flex flex-col justify-center items-center"
-					initial={{ opacity: 0, y: 50 }}
+					className="bg-gray-900 text-gray-100 w-full h-full max-w-screen overflow-y-auto overflow-x-hidden p-4 relative"
+					initial={{ opacity: 0, y: 30 }}
 					animate={{ opacity: 1, y: 0 }}
-					exit={{ opacity: 0, y: 50 }}
+					exit={{ opacity: 0, y: 30 }}
 					transition={{ duration: 0.3 }}
 				>
 					{/* Botão de fechar */}
 					<button
-						className="absolute cursor-pointer top-4 right-4 text-gray-500 hover:text-gray-700"
+						type="button"
+						className="absolute top-4 right-4 text-gray-400 hover:text-white"
 						onClick={onClose}
 					>
 						<X className="w-6 h-6" />
 					</button>
 
-					<h2 className="text-2xl font-bold mb-6 w-full text-left">{title}</h2>
-					<div className="w-full">{children}</div>
+					{/* Título */}
+					<h2 className="text-2xl font-bold mb-6 break-words">{title}</h2>
+
+					{/* Conteúdo com segurança contra overflow */}
+					<div className="pb-10 w-full break-words overflow-x-hidden">
+						{children}
+					</div>
 				</motion.div>
 			</motion.div>
 		</AnimatePresence>
