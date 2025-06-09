f009eae1307fb0f068ac20971d242177d692a267
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-09 22:21:09 +0100

    Arquivo que valida oa página de contato

---
+++ src/validations/contatoValidation.js
@@ -0,0 +1,11 @@
+import { telefoneAngolaRegex } from "./regex";
+
+export const contatoSchema = yup.object().shape({
+  nome: yup.string().required("O nome é obrigatório."),
+  email: yup.string().email("E-mail inválido.").required("O e-mail é obrigatório."),
+  telefone: yup
+    .string()
+    .matches(telefoneAngolaRegex, "Use o formato: +244 923 456 789.")
+    .required("O telefone é obrigatório."),
+  mensagem: yup.string().min(10, "A mensagem deve ter no mínimo 10 caracteres.").required("A mensagem é obrigatória."),
+});
