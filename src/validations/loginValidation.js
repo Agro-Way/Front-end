148d40dec996986e6516f353a7a9a5bef1a305a8
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-10 00:44:47 +0100

    Adição de uma nova validação na senha

--- src/validations/loginValidation.js
+++ src/validations/loginValidation.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const loginSchema = yup.object().shape({
+export const loginValidation  = yup.object().shape({
   email: yup.string().email("E-mail inválido.").required("O e-mail é obrigatório."),
-  senha: yup.string().required("A senha é obrigatória."),
+  senha: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres.").required("A senha é obrigatória."),
 });
