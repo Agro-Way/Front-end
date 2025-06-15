45fd224d90e18b13afafc2cdc9de5c7a26908a2f
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-15 08:36:26 +0100

    Página de motoristainfo criada

---
+++ src/pages/MotoristaInfo.jsx
@@ -0,0 +1,135 @@
+// src/pages/MotoristaInfo.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import "./../assets/css/signup.css";
+//import SignStyle from '@/assets/css/SignStyle';
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { cadastroValidation } from "../validations/cadastroValidation";
+import { toast, ToastContainer } from "react-toastify";
+
+function MotoristaInfo() {
+  useDocumentTitle("Cadastrar Veículo | Agroway");
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(cadastroValidation),
+  });
+
+  const onSubmit = (data) => {
+    try {
+      console.log(data);
+      toast.success("Cadastro feito com sucesso!");
+      reset(); // Limpa o formulário
+    } catch (error) {
+      toast.error("Erro ao fazer cadastro. Tente novamente.");
+    }
+  };
+
+  return (
+    <>
+      {/*Signup*/}
+      <section className="signup" style={{ overflowY: "hidden" }}>
+        <form
+          action=""
+          method="POST"
+          onSubmit={handleSubmit(onSubmit)}
+          className="form-signup"
+        >
+          <h3>Vamos, Faça O Cadastro Do Seu Veículo</h3>
+
+          <div className="inputBox">
+            <input
+              type="text"
+              {...register("nome")}
+              name="nome"
+              placeholder="Seu nome"
+              className="box"
+            />
+            <p className="error-msg">{errors.nome?.message}</p>
+          </div>
+          <div className="inputBox">
+            <input
+              type="email"
+              {...register("email")}
+              name="email"
+              placeholder="exemplo@gmail.com"
+              className="box"
+            />
+            <p className="error-msg">{errors.email?.message}</p>
+          </div>
+
+          <div className="inputBox">
+            <input
+              type="tel"
+              name="tel"
+              {...register("telefone")}
+              placeholder="Seu telefone"
+              className="box"
+            />
+            <p className="error-msg">{errors.telefone?.message}</p>
+          </div>
+
+          <div className="inputBox">
+            <select
+              id="funcao"
+              {...register("funcao")}
+              name="funcao"
+              className="box"
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Selecione sua função
+              </option>
+              <option value="1">Cliente</option>
+              <option value="2">Motorista</option>
+              <option value="3">Produtor</option>
+            </select>
+            <p className="error-msg">{errors.funcao?.message}</p>
+          </div>
+
+          <div className="inputBox">
+            <input
+              type="password"
+              {...register("senha")}
+              name="senha"
+              placeholder="Sua palavra-pass"
+              className="box"
+            />
+            <p className="error-msg">{errors.senha?.message}</p>
+          </div>
+
+          <div className="inputBox">
+            <input
+              type="password"
+              {...register("confirmarSenha")}
+              name="confirmarSenha"
+              placeholder="Confirme sua palavra-pass"
+              className="box"
+            />
+            <p className="error-msg">{errors.confirmarSenha?.message}</p>
+          </div>
+
+          <button type="submit" className="btn">
+            Cadastrar Veículo
+          </button>
+        </form>
+      </section>
+
+      {/* Container de Notificações */}
+      <ToastContainer
+        toastClassName="toast-tam"
+        position="top-right"
+        autoClose={4000}
+      />
+    </>
+  );
+}
+
+export default MotoristaInfo;
