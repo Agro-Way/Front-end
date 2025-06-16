703c364cabf4ab0cd6893e25ac85b3d895bafb55
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-16 11:18:08 +0100

    Atualizações pontuais feitas

--- src/validations/cadastroValidation.js
+++ src/validations/cadastroValidation.js
@@ -3,21 +3,17 @@ import { telefoneAngolaRegex, passwordRegex } from "./regex";
 
 export const cadastroValidation = yup.object().shape({
   nome: yup.string().required("O nome é obrigatório."),
-  email: yup.string().email("E-mail inválido.").required("O e-mail é obrigatório."),
+  email: yup.string().required("O e-mail é obrigatório.").email("E-mail inválido."),
   telefone: yup
-    .string()
-    .matches(telefoneAngolaRegex, "Use o formato: +244 923 456 789.")
-    .required("O telefone é obrigatório."),
+    .string().required("O telefone é obrigatório.")
+    .matches(telefoneAngolaRegex, "Use o formato: +244 923 456 789."),
   senha: yup
-    .string()
-    .matches(passwordRegex, "A senha deve conter letras e números, mínimo 8 caracteres.")
-    .required("A senha é obrigatória."),
+    .string().required("A senha é obrigatória.")
+    .matches(passwordRegex, "A senha deve conter letras e números, mínimo 8 caracteres."),
   confirmarSenha: yup
-    .string()
-    .oneOf([yup.ref("senha")], "As senhas não coincidem.")
-    .required("A confirmação da senha é obrigatória."),
+    .string().required("A confirmação da senha é obrigatória.")
+    .oneOf([yup.ref("senha")], "As senhas não coincidem."),
   funcao: yup
-    .string()
-    .oneOf(["1", "2", "3"], "Selecione uma função válida.")
-    .required("A função é obrigatória."),
+    .string().required("A função é obrigatória.")
+    .oneOf(["1", "2", "3"], "Selecione uma função válida."),
 });
--- src/validations/loginValidation.js
+++ src/validations/loginValidation.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const loginValidation  = yup.object().shape({
-  email: yup.string().email("E-mail inválido.").required("O e-mail é obrigatório."),
-  senha: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres.").required("A senha é obrigatória."),
+  email: yup.string().required("O e-mail é obrigatório.").email("E-mail inválido."),
+  senha: yup.string().required("A senha é obrigatória.").min(8, "A senha deve ter no mínimo 8 caracteres."),
 });
