558104bafe2a67f60976423e7558c0b56b4ec06a
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-18 13:07:02 +0100

    Atualizado com ReactDOM para criar um portal e exibir o modal

--- src/dashboard/components/common/Modal.jsx
+++ src/dashboard/components/common/Modal.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom";//mporta o utilitário necessário para criar um portal.
 import { motion, AnimatePresence } from "framer-motion";
 
 const Modal = ({ isOpen, onClose, children }) => {
-  return (
+  //Garante que o componente não quebre em ambiente SSR (como Next.js).
+  if (typeof window === "undefined") return null;
+
+  //Garante que o modal será renderizado fora da árvore do componente pai, diretamente no <body> da página.
+  return ReactDOM.createPortal(
     <AnimatePresence>
       {isOpen && (
         <div
-          className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center"
+          className="fixed inset-0 z-50 bg-black/50 text-gray-100  bg-opacity-50 flex justify-center items-center"
           onClick={onClose}
         >
           <motion.div
@@ -17,7 +22,8 @@ const Modal = ({ isOpen, onClose, children }) => {
             exit={{ opacity: 0, scale: 0.95 }}
             transition={{ duration: 0.2 }}
           >
-            <button type="button"
+            <button
+              type="button"
               onClick={onClose}
               className="absolute cursor-pointer top-3 right-3 text-gray-400 hover:text-red-500 text-xl"
             >
@@ -27,7 +33,8 @@ const Modal = ({ isOpen, onClose, children }) => {
           </motion.div>
         </div>
       )}
-    </AnimatePresence>
+    </AnimatePresence>,
+    document.body
   );
 };
 
