90d37f59d1ce131ad5368b5ca04fbe331f92f96e
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-20 13:30:09 +0100

    Mock de dados de blogs criado

---
+++ src/components/DadosBlogs.js
@@ -0,0 +1,13 @@
+import blogImg1 from "../assets/img/blog1.png";
+import blogImg2 from "../assets/img/blog2.png";
+import blogImg3 from "../assets/img/blog3.png";
+
+const blogs = [
+    {id: 1, imagem: blogImg1, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin",  desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {id: 2, imagem: blogImg2, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin", desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {id: 3, imagem: blogImg3, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin",  desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {id: 4, imagem: blogImg1, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin",  desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {id: 5, imagem: blogImg2, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin", desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    {id: 6, imagem: blogImg3, titulo: "Vegetais Saudáveis Para Experimentar", autor:"Admin",  desc: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+]
+export default blogs
