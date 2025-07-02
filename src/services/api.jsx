5302decd10fdd5702bf32aa673e93b2c14b8fed9
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-07-02 21:56:03 +0100

    Serviço centralizado que pega o caminho da url base da API criado

---
+++ src/services/api.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+/**
+ * Serviço centralizado para armazenar o caminho base da API e reutilizá-lo
+ * nas páginas/componentes para fazer chamadas HTTP.
+ */
+
+const api = axios.create({
+    baseURL: "https://agro-way-api.onrender.com",
+    timeout: 10,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+export default api;
