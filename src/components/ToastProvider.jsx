f5429677f20bc91cfeec40f7a414050aa9bddc74
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-07-04 00:18:29 +0100

    Criado componente para mostrar mensagens ao adicionar e remover produtos do carrinho

---
+++ src/components/ToastProvider.jsx
@@ -0,0 +1,18 @@
+// src/components/ToastProvider.jsx
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export function ToastProvider() {
+  return (
+    <ToastContainer
+      toastClassName="toast-tam"
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="light"
+    />
+  );
+}
